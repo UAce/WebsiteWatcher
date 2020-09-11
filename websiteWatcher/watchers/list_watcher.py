a85b386af1ce86d5dde1ee090ab1eb30ddbe1c9f
Yu-Yueh Liu <yueh.liu@ormuco.com>
2020-09-11 01:07:40 -0400

    add take_screenshot functioN

--- websiteWatcher/watchers/list_watcher.py
+++ websiteWatcher/watchers/list_watcher.py
@@ -1,4 +1,5 @@
 import logging
+from PIL import Image
 from bs4 import BeautifulSoup, element
 
 from websiteWatcher.common.utils import send_email
@@ -34,6 +35,7 @@ class ListWatcher(BaseWatcher):
             self.options["target_attr"],
             self.options["target_attr_val"],
         )
+        self.take_screenshot()
         if self.initial_count is None:
             self.initial_count = current_count
             self.current_count = self.initial_count
@@ -85,3 +87,21 @@ class ListWatcher(BaseWatcher):
             """
 
             send_email(f"{self.name} News!", content)
+
+    def take_screenshot(self):
+        list_tag = self.options["list_tag"]
+        list_attr = self.options["list_attr"]
+        list_attr_val = self.options["list_attr_val"]
+        target_list = self.driver.find_element_by_xpath(
+            f"//{list_tag}[contains(@{list_attr}, '{list_attr_val}')]"
+        )
+
+        if target_list.is_displayed():
+            self.driver.get_screenshot_as_file("list-watcher-page.png")
+            left = int(target_list.location["x"])
+            top = int(target_list.location["y"])
+            right = int(target_list.location["x"] + target_list.size["width"])
+            bottom = int(target_list.location["y"] + target_list.size["height"])
+            im = Image.open("list-watcher-page.png")
+            im = im.crop((left, top, right, bottom))
+            im.save("list-watcher-target.png")
